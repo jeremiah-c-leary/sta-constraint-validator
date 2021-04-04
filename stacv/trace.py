e10ba884efb23d7c2a561bf3707df63c489fdf82
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2021-04-03 20:21:21 -0500

    Adding trace class and tests.

---
+++ stacv/trace.py
@@ -0,0 +1,31 @@
+
+class New():
+
+    def __init__(self, name, device_pin, part_pin, max_delay, min_delay):
+        self.name = name
+        self.device_pin = device_pin
+        self.part_pin = part_pin
+        self.min_delay = min_delay
+        self.max_delay = max_delay
+
+    @property
+    def min_delay(self):
+        return self.__min_delay
+
+    @min_delay.setter
+    def min_delay(self, min_delay):
+        if min_delay < 0:
+            raise ValueError
+        else:
+            self.__min_delay = min_delay
+
+    @property
+    def max_delay(self):
+        return self.__max_delay
+
+    @max_delay.setter
+    def max_delay(self, max_delay):
+        if max_delay < self.__min_delay:
+            raise ValueError
+        else:
+            self.__max_delay = max_delay
---
+++ tests/test_trace.py
@@ -0,0 +1,42 @@
+
+import unittest
+
+from stacv import trace
+
+
+class Test(unittest.TestCase):
+
+    def test_new_trace(self):
+        sName = 'trace'
+        sDevicePin = 'device_pin'
+        sPartPin = 'part_pin'
+        fMaxDelay = 1
+        fMinDelay = 0.567
+
+        oTrace = trace.New(sName, sDevicePin, sPartPin, fMaxDelay, fMinDelay)
+
+        self.assertEqual(sName, oTrace.name)
+        self.assertEqual(sDevicePin, oTrace.device_pin)
+        self.assertEqual(sPartPin, oTrace.part_pin)
+        self.assertEqual(fMaxDelay, oTrace.max_delay)
+        self.assertEqual(fMinDelay, oTrace.min_delay)
+
+        blah2 = oTrace.max_delay + oTrace.min_delay
+
+    def test_min_delay_less_than_0(self):
+
+        sName = 'trace'
+        sDevicePin = 'device_pin'
+        sPartPin = 'part_pin'
+        fMaxDelay = 10
+        fMinDelay = -1
+
+        self.assertRaises(ValueError, trace.New, sName, sDevicePin, sPartPin, fMaxDelay, -1)
+
+    def test_max_delay_less_than_min_delay(self):
+
+        sName = 'trace'
+        sDevicePin = 'device_pin'
+        sPartPin = 'part_pin'
+
+        self.assertRaises(ValueError, trace.New, sName, sDevicePin, sPartPin, 1, 2)
