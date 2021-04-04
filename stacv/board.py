96c21360d74bafe06014ecf6bb7f9652bb1f7790
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2021-04-03 21:18:14 -0500

    Adding board factory, class and test.

---
+++ stacv/board.py
@@ -0,0 +1,36 @@
+
+from . import trace
+
+
+def New(board_config):
+
+    board = Board()
+
+    for trace_name in board_config['board']['trace']:
+
+        trace_args = extract_trace_data(trace_name, board_config)
+
+        new_trace = trace.New(*trace_args)
+
+        board.add_trace(new_trace)
+
+    return board
+
+
+def extract_trace_data(trace_name, board_config):
+    trace_config = board_config['board']['trace'][trace_name]
+    device_pin = trace_config['device_pin']
+    part_pin = trace_config['part_pin']
+    delay_max = trace_config['delay']['max']
+    delay_min = trace_config['delay']['min']
+    return trace_name, device_pin, part_pin, delay_max, delay_min
+
+
+class Board():
+
+    def __init__(self):
+        self.traces = []
+
+    def add_trace(self, trace):
+        self.traces.append(trace)
+   
---
+++ tests/board/test_board.py
@@ -0,0 +1,47 @@
+
+import unittest
+
+from stacv import board
+
+
+
+dBoardConfig = {}
+dBoardConfig['board'] = {}
+dBoardConfig['board']['trace'] = {}
+
+dTrace = {}
+dTrace['device_pin'] = 'D1'
+dTrace['part_pin'] = 'P1'
+dTrace['delay'] = {}
+dTrace['delay']['max'] = 5.0
+dTrace['delay']['min'] = 5.0
+
+dBoardConfig['board']['trace']['first'] = dTrace
+
+dTrace = {}
+dTrace['device_pin'] = 'D2'
+dTrace['part_pin'] = 'P2'
+dTrace['delay'] = {}
+dTrace['delay']['max'] = 4.0
+dTrace['delay']['min'] = 2.0
+
+dBoardConfig['board']['trace']['second'] = dTrace
+
+dTrace = {}
+dTrace['device_pin'] = 'D3'
+dTrace['part_pin'] = 'P3'
+dTrace['delay'] = {}
+dTrace['delay']['max'] = 6.0
+dTrace['delay']['min'] = 3.0
+
+dBoardConfig['board']['trace']['third'] = dTrace
+
+
+class Test(unittest.TestCase):
+
+
+    def test_new_board(self):
+ 
+        oBoard = board.New(dBoardConfig)
+
+        self.assertEqual(3, len(oBoard.traces))
