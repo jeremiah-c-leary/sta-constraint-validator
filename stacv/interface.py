9a38606a427f8c472e9299f35a14436488e39857
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2021-04-10 09:17:44 -0500

    Removing unused code form clock diagram. Refactoring variable names in interface.py

--- stacv/interface.py
+++ stacv/interface.py
@@ -48,14 +48,14 @@ def has_output_clocks(interface_dict):
 def build_part_input_clock_pin_list(interface_dict):
     clock_pins = []
     for my_pin in interface_dict['clock']['input']:
-        dClock = {}
-        dClock['location'] = 'part'
-        dClock['direction'] = 'input'
-        dClock['pin_type'] = 'clock'
+        clock_dict = {}
+        clock_dict['location'] = 'part'
+        clock_dict['direction'] = 'input'
+        clock_dict['pin_type'] = 'clock'
         name = list(my_pin.keys())[0]
-        dClock['name'] = name
-        dClock['max_freq'] = my_pin[name]['max_freq']
-        clock_pins.append(pin.new(dClock))
+        clock_dict['name'] = name
+        clock_dict['max_freq'] = my_pin[name]['max_freq']
+        clock_pins.append(pin.new(clock_dict))
 
     return clock_pins
 
@@ -63,14 +63,14 @@ def build_part_input_clock_pin_list(interface_dict):
 def build_part_output_clock_pin_list(interface_dict):
     clock_pins = []
     for my_pin in interface_dict['clock']['output']:
-        dClock = {}
-        dClock['location'] = 'part'
-        dClock['direction'] = 'output'
-        dClock['pin_type'] = 'clock'
+        clock_dict = {}
+        clock_dict['location'] = 'part'
+        clock_dict['direction'] = 'output'
+        clock_dict['pin_type'] = 'clock'
         name = list(my_pin.keys())[0]
-        dClock['name'] = name
-        dClock['max_freq'] = my_pin[name]['max_freq']
-        clock_pins.append(pin.new(dClock))
+        clock_dict['name'] = name
+        clock_dict['max_freq'] = my_pin[name]['max_freq']
+        clock_pins.append(pin.new(clock_dict))
 
     return clock_pins
 
@@ -99,18 +99,18 @@ def has_output_data_pins(interface_dict):
 def build_part_input_data_pin_list(interface_dict):
     data_pins = []
     for my_pin in interface_dict['data']['input']:
-        dPin = {}
-        dPin['location'] = 'part'
-        dPin['direction'] = 'input'
-        dPin['pin_type'] = 'data'
+        pin_dict = {}
+        pin_dict['location'] = 'part'
+        pin_dict['direction'] = 'input'
+        pin_dict['pin_type'] = 'data'
         name = list(my_pin.keys())[0]
-        dPin['name'] = name
-        dPin['clock'] = my_pin[name]['clock']
+        pin_dict['name'] = name
+        pin_dict['clock'] = my_pin[name]['clock']
         if has_rising_edge(my_pin[name]):
-            dPin['rising'] = build_part_input_edge(my_pin[name]['rising'])
+            pin_dict['rising'] = build_part_input_edge(my_pin[name]['rising'])
         if has_falling_edge(my_pin[name]):
-            dPin['falling'] = build_part_input_edge(my_pin[name]['falling'])
-        data_pins.append(pin.new(dPin))
+            pin_dict['falling'] = build_part_input_edge(my_pin[name]['falling'])
+        data_pins.append(pin.new(pin_dict))
     return data_pins
 
 
@@ -136,18 +136,18 @@ def build_part_input_edge(edge_dict):
 def build_part_output_data_pin_list(interface_dict):
     data_pins = []
     for my_pin in interface_dict['data']['output']:
-        dPin = {}
-        dPin['location'] = 'part'
-        dPin['direction'] = 'output'
-        dPin['pin_type'] = 'data'
+        pin_dict = {}
+        pin_dict['location'] = 'part'
+        pin_dict['direction'] = 'output'
+        pin_dict['pin_type'] = 'data'
         name = list(my_pin.keys())[0]
-        dPin['name'] = name
-        dPin['clock'] = my_pin[name]['clock']
+        pin_dict['name'] = name
+        pin_dict['clock'] = my_pin[name]['clock']
         if has_rising_edge(my_pin[name]):
-            dPin['rising'] = build_part_output_edge(my_pin[name]['rising'])
+            pin_dict['rising'] = build_part_output_edge(my_pin[name]['rising'])
         if has_falling_edge(my_pin[name]):
-            dPin['falling'] = build_part_output_edge(my_pin[name]['falling'])
-        data_pins.append(pin.new(dPin))
+            pin_dict['falling'] = build_part_output_edge(my_pin[name]['falling'])
+        data_pins.append(pin.new(pin_dict))
     return data_pins
 
 
