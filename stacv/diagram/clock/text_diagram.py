f4c1711207b764f5b8d6a71098ff422d586c65d9
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2021-04-10 17:30:14 -0500

    Refactoring text clock diagram to improve readability.

--- stacv/diagram/clock/text_diagram.py
+++ stacv/diagram/clock/text_diagram.py
@@ -1,5 +1,6 @@
 
 from stacv import clock
+from stacv import utils
 
 
 def render(timing_model, pin):
@@ -8,14 +9,12 @@ def render(timing_model, pin):
 
     launch_clock, capture_clock = build_clocks(timing_model, pin)
 
-    launch_dwell_time, capture_dwell_time = calculate_dwell_times(launch_clock, capture_clock)
-
-    diagram.extend(render_clock(launch_clock, launch_dwell_time))
+    diagram.extend(render_clock(launch_clock))
     diagram.extend(render_blank_line())
-    diagram.extend(render_setup_arc(launch_clock, launch_dwell_time, capture_clock, capture_dwell_time))
-    diagram.extend(render_hold_arc(launch_clock, launch_dwell_time, capture_clock, capture_dwell_time))
+    diagram.extend(render_setup_arc(launch_clock, capture_clock))
+    diagram.extend(render_hold_arc(launch_clock, capture_clock))
     diagram.extend(render_blank_line())
-    diagram.extend(render_clock(capture_clock, capture_dwell_time, letter=True))
+    diagram.extend(render_clock(capture_clock, letter=True))
 
     return diagram
 
@@ -27,9 +26,12 @@ def build_clocks(timing_model, pin):
 
     launch_clock = clock.new(launch_clock_pin.frequency)
     launch_clock.edge = device_data_pin.launch_edge
+
     capture_clock = clock.new(capture_clock_pin.frequency)
     capture_clock.setup_edge = device_data_pin.setup_edge
     capture_clock.hold_edge = device_data_pin.hold_edge
+
+    calculate_dwell_times(launch_clock, capture_clock)
 
     expand_clocks(launch_clock, capture_clock)
 
@@ -48,48 +50,80 @@ def expand_clocks(launch_clock, capture_clock):
         clock.expand(capture_clock, fast_clock_expansion)
 
 
-def render_clock(launch_clock, dwell_time, letter=False):
+def render_clock(launch_clock, letter=False):
     lines = []
 
-
-    if not letter:
-        if len(launch_clock.edges) > 10:
-            line = '   '
-            for edge_number, edge in enumerate(launch_clock.edges[:-1]):
-               edge_number = int((edge_number + 2) / 10)
-               if edge_number > 0:
-                   line += ' '*dwell_time + str(edge_number)
-               else:
-                   line += ' '*dwell_time + ' '
-            lines.append(line)
-
-        line = '  1'
-        for edge_number, edge in enumerate(launch_clock.edges[:-1]):
-           if edge.direction == 'rising':
-               line += '_'*dwell_time + str((edge_number + 2)%10)
-           else:
-               line += ' '*dwell_time + str((edge_number + 2)%10)
-        lines.append(line)
-    else:
-        line = '  a'
-        for edge_number, edge in enumerate(launch_clock.edges[:-1]):
-           if edge.direction == 'rising':
-               line += '_'*dwell_time + str(conv_number_to_letter(edge_number + 2))
-           else:
-               line += ' '*dwell_time + str(conv_number_to_letter(edge_number + 2))
-        lines.append(line)
-
-    line = '__'
-    for edge in launch_clock.edges[:-1]:
-       if edge.direction == 'rising':
-           line += '|' + ' '*dwell_time
-       else:
-           line += '|' + '_'*dwell_time
-    line += '|'
-
-    lines.append(line)
+    lines.extend(render_edge_ids(launch_clock, letter))
+    lines.append(render_clock_edges(launch_clock))
 
     return lines
+
+
+def render_edge_ids(launch_clock, letter):
+    lines = []
+    if not letter:
+        render_number_line(launch_clock, lines)
+    else:
+        render_letter_line(launch_clock, lines)
+    return lines
+
+
+def render_number_line(launch_clock, lines):
+    render_tens_line(launch_clock, lines)
+    render_ones_line(launch_clock, lines)
+
+
+def render_letter_line(launch_clock, lines):
+    line = '  a'
+    for edge_number, edge in enumerate(launch_clock.edges[:-1]):
+       if edge.direction == 'rising':
+           line += '_'*launch_clock.dwell_time + str(utils.conv_number_to_letter(edge_number + 2))
+       else:
+           line += ' '*launch_clock.dwell_time + str(utils.conv_number_to_letter(edge_number + 2))
+    lines.append(line)
+
+
+def render_ones_line(launch_clock, lines):
+    line = '  1'
+    for edge_number, edge in enumerate(launch_clock.edges[:-1]):
+       if edge.direction == 'rising':
+           line += '_'*launch_clock.dwell_time + str((edge_number + 2)%10)
+       else:
+           line += ' '*launch_clock.dwell_time + str((edge_number + 2)%10)
+    lines.append(line)
+
+
+def render_tens_line(launch_clock, lines):
+    if len(launch_clock.edges) > 10:
+        line = '   '
+        for edge_number, edge in enumerate(launch_clock.edges[:-1]):
+           edge_number = int((edge_number + 2) / 10)
+           if edge_number > 0:
+               line += ' '*launch_clock.dwell_time + str(edge_number)
+           else:
+               line += ' '*launch_clock.dwell_time + ' '
+        lines.append(line)
+
+
+def render_clock_edges(launch_clock):
+    line = '__'
+    for edge in launch_clock.edges[:-1]:
+       line = render_rising_clock_edge(edge, launch_clock, line)
+       line = render_falling_clock_edge(edge, launch_clock, line)
+    line += '|'
+    return line
+
+
+def render_rising_clock_edge(edge, launch_clock, line):
+       if edge.direction == 'rising':
+           line += '|' + ' '*launch_clock.dwell_time
+       return line
+
+
+def render_falling_clock_edge(edge, launch_clock, line):
+       if edge.direction == 'falling':
+           line += '|' + '_'*launch_clock.dwell_time
+       return line
 
 
 def render_blank_line():
@@ -98,14 +132,12 @@ def render_blank_line():
 
 def calculate_dwell_times(launch_clock, capture_clock):
     if launch_clock.period < capture_clock.period:
-        launch_dwell_time, capture_dwell_time = calculate_fast_and_slow_dwell_times(launch_clock, capture_clock)
+        launch_clock.dwell_time, capture_clock.dwell_time = calculate_fast_and_slow_dwell_times(launch_clock, capture_clock)
     else:
-        capture_dwell_time, launch_dwell_time = calculate_fast_and_slow_dwell_times(capture_clock, launch_clock)
-    return launch_dwell_time, capture_dwell_time
+        capture_clock.dwell_time, launch_clock.dwell_time = calculate_fast_and_slow_dwell_times(capture_clock, launch_clock)
 
 
-def calculate_fast_and_slow_dwell_times(fast_clock, slow_clock):
-    fast_dwell_time = 2
+def calculate_fast_and_slow_dwell_times(fast_clock, slow_clock, fast_dwell_time=2):
 
     ratio = int(slow_clock.period/fast_clock.period)
 
@@ -114,13 +146,13 @@ def calculate_fast_and_slow_dwell_times(fast_clock, slow_clock):
     return fast_dwell_time, slow_dwell_time
 
 
-def render_setup_arc(launch_clock, launch_dwell_time, capture_clock, capture_dwell_time):
+def render_setup_arc(launch_clock, capture_clock):
     lines = []
 
-    capture_edge = conv_letter_to_number(capture_clock.setup_edge)
+    capture_edge = utils.conv_letter_to_number(capture_clock.setup_edge)
 
-    launch_edge_index = (launch_dwell_time + 1) * (launch_clock.edge - 1) + 3- 1
-    capture_edge_index = (capture_dwell_time + 1) * (capture_edge - 1) + 3 - 1
+    launch_edge_index = (launch_clock.dwell_time + 1) * (launch_clock.edge - 1) + 3- 1
+    capture_edge_index = (capture_clock.dwell_time + 1) * (capture_edge - 1) + 3 - 1
     delta = capture_edge_index - launch_edge_index - 2
     
     line = ' '*launch_edge_index + '|' + '-'*delta + '>|'
@@ -130,27 +162,16 @@ def render_setup_arc(launch_clock, launch_dwell_time, capture_clock, capture_dwe
     return lines
 
 
-def render_hold_arc(launch_clock, launch_dwell_time, capture_clock, capture_dwell_time):
+def render_hold_arc(launch_clock, capture_clock):
     lines = []
 
-    capture_edge = conv_letter_to_number(capture_clock.hold_edge)
+    capture_edge = utils.conv_letter_to_number(capture_clock.hold_edge)
 
-    launch_edge_index = (launch_dwell_time + 1) * (launch_clock.edge - 1) + 3- 1
-    capture_edge_index = (capture_dwell_time + 1) * (capture_edge - 1) + 3 - 1
+    launch_edge_index = (launch_clock.dwell_time + 1) * (launch_clock.edge - 1) + 3- 1
+    capture_edge_index = (capture_clock.dwell_time + 1) * (capture_edge - 1) + 3 - 1
     delta = launch_edge_index - capture_edge_index - 2
     
     line = ' '*capture_edge_index + '|<' + '-'*delta + '|'
     lines.append(line)
 
     return lines
-
-
-letter_mapping_table = [None, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-
-def conv_letter_to_number(letter):
-    return letter_mapping_table.index(letter)
-
-
-def conv_number_to_letter(number):
-    return letter_mapping_table[number]
--- stacv/utils.py
+++ stacv/utils.py
@@ -17,3 +17,14 @@ def read_config_file(config_files):
         my_file = {}
 
     return my_file
+
+
+letter_mapping_table = [None, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def conv_letter_to_number(letter):
+    return letter_mapping_table.index(letter)
+
+
+def conv_number_to_letter(number):
+    return letter_mapping_table[number]
